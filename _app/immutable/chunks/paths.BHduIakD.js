58d2862f3cd120ce08261713790827d2c21bcc87
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-09-19 19:47:26 +0300

    Deploy

--- _app/immutable/chunks/paths.BHduIakD.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_a201ge)==null?void 0:s.base)??"/elite-club";var e;const t=((e=globalThis.__sveltekit_a201ge)==null?void 0:e.assets)??a;export{t as a,a as b};
